1f29a2e92fa2ea513ffc3b2a626788f186bdae4a
FSaal <f.saalfrank@gmail.com>
2022-01-11 12:53:54 +0100

    Add commandline arguments

--- code/fusion/ros/get_tfs.py
+++ code/fusion/ros/get_tfs.py
@@ -4,6 +4,7 @@ from numpy.lib.utils import source
 import rospy
 import numpy as np
 import tf
+import sys
 from tf.listener import TransformListener
 from tf.transformations import euler_from_quaternion
 
@@ -11,17 +12,21 @@ class FusionRamp():
     def __init__(self):
         rospy.init_node('tf_test', anonymous=True)
         self.tf = TransformListener()
+        if len(sys.argv) != 3:
+            print('Two command line arguments must be passed')
+            print('First argument is source_frame and second is target_frame')
+            sys.exit()
 
     def spin(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            target_frame = '/zed2i_left_camera_optical_frame'
-            source_frame = '/lidar'
+            source_frame = sys.argv[1]
+            target_frame = sys.argv[2]
 
-            # canTransform = self.tf.canTransform(target_frame, source_frame, rospy.Time.now())
-            # canTransform = '' if canTransform else 'not'
-            # print('Transformation from {} to {} frame is {} possible'.format(
-            #     source_frame, target_frame, canTransform))
+            canTransform = self.tf.canTransform(target_frame, source_frame, rospy.Time.now())
+            canTransform = '' if canTransform else 'not'
+            print('Transformation from {} to {} frame is {} possible'.format(
+                source_frame, target_frame, canTransform))
 
             try:
                 (trans, rot) = self.tf.lookupTransform(target_frame, source_frame, rospy.Time.now())
